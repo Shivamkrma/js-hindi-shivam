56441934dd6f28aa48c375d5b8c5c16fefd84cd0
Shivamkrma <130297619+Shivamkrma@users.noreply.github.com>
2023-11-25 06:01:48 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountid=12345;
+var shivam=345;
+let accountpassword="2345556";
+shivam=459;
+accountpassword="636265261";
+console.table([accountpassword,shivam])
+console.log(typeof undefined)
